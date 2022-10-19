40a937e7ad97bfff10ca019dc4778b9290f594b9
tyson lind <tysonfloydlind@gmail.com>
2022-10-19 13:41:52 -0600

    committing assignment

--- app.js
+++ app.js
@@ -4,7 +4,27 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName;
+let lastName = "Lind";
+var age = 75;
 // Exercise 2
-
+firstName = "Tyson";
+lastname = "Lind";
+age = 75;
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
